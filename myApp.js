16a2eee4cbc5a792ba503c3eedc40a3f6b922abe
Ancientshield <jacklin2100@gmail.com>
2023-04-19 20:49:40 +0800

    use app.get('/', ...) to send msg

--- myApp.js
+++ myApp.js
@@ -1,39 +1,9 @@
 let express = require('express');
 let app = express();
 
+// req, res
+app.get('/', (req, res) => {
+	res.send('Response String');
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
