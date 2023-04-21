093a4bdc349ef6a604cb0fd9c8de26f217dcbd15
Ancientshield <jacklin2100@gmail.com>
2023-04-21 22:29:54 +0800

    Serve JSON on a Specific Route

--- myApp.js
+++ myApp.js
@@ -4,10 +4,18 @@ let app = express();
 // use middleware app.use(path, middlewareFunction) to serve static assets.
 app.use('/public', express.static(__dirname + '/public'));
 
-// req, res
+// request, response
 app.get('/', (req, res) => {
 	// res.send('Hello Express');
 	res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/json', (req, res) => {
+	let message = 'Hello json';
+	if (process.env.MESSAGE_STYLE === 'uppercase') {
+		return res.json({ message: message.toUpperCase() });
+	}
+	return res.status(200).json({ message: message });
 });
 
 module.exports = app;
