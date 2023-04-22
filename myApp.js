283ccf9076dfe057b0650117b21aba68014798fa
Ancientshield <jacklin2100@gmail.com>
2023-04-22 22:31:17 +0800

    use npm dotenv to setting .env and response json

--- myApp.js
+++ myApp.js
@@ -1,14 +1,11 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
+let proc = process.env;
+proc.MESSAGE_STYLE = 'uppercase';
 
 // use middleware app.use(path, middlewareFunction) to serve static assets.
 app.use('/public', express.static(__dirname + '/public'));
-
-// request, response
-app.get('/', (req, res) => {
-	// res.send('Hello Express');
-	res.sendFile(__dirname + '/views/index.html');
-});
 
 app.get('/json', (req, res) => {
 	let message = 'Hello json';
@@ -16,6 +13,11 @@ app.get('/json', (req, res) => {
 		return res.json({ message: message.toUpperCase() });
 	}
 	return res.status(200).json({ message: message });
+});
+// request, response
+app.get('/', (req, res) => {
+	// res.send('Hello Express');
+	res.sendFile(__dirname + '/views/index.html');
 });
 
 module.exports = app;
