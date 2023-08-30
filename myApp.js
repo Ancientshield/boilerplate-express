ab1288a5c9a008c511f53a04ab5c2d2396ef8620
Ancientshield <ancientsword520@gmail.com>
2023-08-30 11:33:00 +0800

    finished 007 Implement a Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -7,6 +7,12 @@ proc.MESSAGE_STYLE = 'uppercase';
 // use middleware app.use(path, middlewareFunction) to serve static assets.
 app.use('/public', express.static(__dirname + '/public'));
 
+// Mount the Logger middleware here
+app.use(function middleware(req, res, next) {
+	console.log(`${req.method} ${req.path} - ${req.ip}`);
+	next();
+});
+
 app.get('/json', (req, res) => {
 	let message = 'Hello json';
 	if (process.env.MESSAGE_STYLE === 'uppercase') {
